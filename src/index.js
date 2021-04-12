431b0ffdd06b3640967f2d0736fdb4d8046349e9
Evan <wolfysonin@gmail.com>
2021-04-11 23:42:40 -0600

    Improved token system

--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
 const Discord = require('discord.js');
+const prompt = require('prompt-sync')();
+const colors = require('colors');
 const client = new Discord.Client();
 const config = require("config"); 
-var token = config.get("token");
-console.log('Token: ', '...' + token.slice(32))
+
+try {
+  var token = config.get('token');
+} // Attempt to read token from config/config.json
+catch(error) {
+  console.log("\nTried to read token from config file, but failed. Please enter a token, or add it to the config and restart the program.".yellow)
+  var token = prompt('Token: ')
+} // If a token was not found, ask for one instead
+
+console.log('Received token: ', '...' + token.slice(32)) // Print out received token, cutting off the first two sections
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-});
+}); // Log the bot account that was signed into
 
 client.on('message', msg => {
   if (msg.content === 'ping') {
     msg.send('pong');
   }
-});
+}); // Example Discord.JS stuff that will be removed
 
-client.login(token);
+client.login(token); 
+
+
