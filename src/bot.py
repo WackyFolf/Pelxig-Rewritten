76ebbe7a7d1409c3d60ec4cd6822e8e330ef724b
Evan <wolfysonin@gmail.com>
2021-04-12 22:12:14 -0600

    New config, more initialization

--- src/bot.py
+++ src/bot.py
@@ -1,6 +1,19 @@
 import discord
+import json
 from discord.ext import commands
 
-bot = commands.Bot(command_prefix='$')
+client = commands.Bot(command_prefix='$')
 
-bot.run('no token yet')
+
+with open('config/botconfig.json') as f:
+    config = json.load(f) # Load config file from config/botconfig.json
+TOKEN = config['token']
+VERSION = config['version']
+UDPATED = config['updated'] # Set respective variables
+
+@client.event
+async def on_ready():
+    await client.change_presence(status=discord.Status.idle, activity=(discord.Activity(type=3,name="over the server as I sit here with no other function but display this status"))) # :)
+    print("Logged in as {0.user}".format(client))
+
+client.run(TOKEN)
